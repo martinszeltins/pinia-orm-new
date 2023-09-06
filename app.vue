18825fb13ae62f3e65c46bb12d631a9c96a25ae3
Martins <34019878+martinszeltins@users.noreply.github.com>
2023-09-06 09:49:49 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ users }}
+  </div>
+
+  <div>
+    <button @click="addUser">
+        Add user
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+    import User from './stores/models/user'
+    import { useRepo } from 'pinia-orm'
+
+    const userRepo = useRepo(User)
+
+    // Getting all users
+    const users = userRepo.all()
+
+    const addUser = () => {
+        userRepo.insert({
+            name: 'John Doe',
+            email: 'john@doe.com'
+        })
+    }
+</script>
